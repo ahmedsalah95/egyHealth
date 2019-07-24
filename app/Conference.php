8bfced5881cd43f75902741b2ea9627f01bad024
ahmed magdy <a.magdi65@gamil.com>
2019-07-24 14:34:15 +0200

    fix

--- app/Conference.php
+++ app/Conference.php
@@ -6,5 +6,23 @@ use Illuminate\Database\Eloquent\Model;
 
 class Conference extends Model
 {
-    //
+    protected $fillable = [
+       'organizationName',
+       'city',
+       'country',
+       'website',
+       'fax',
+       'designation',
+       'state',
+       'email',
+       'phone',
+       'mobile',
+       'firstName',
+       'lastName',
+       'addressLine1',
+       'addressLine2',
+       'alternateEmail',
+       'conferenceTitle',
+       'title',
+    ];
 }
